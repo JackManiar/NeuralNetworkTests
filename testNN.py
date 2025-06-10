e44311294af7a129cb559c8be08703adcb470003
JackManiar <jackmmaniar@gmail.com>
2025-06-10 15:07:40 -0400

    Added training loop and some test points

--- testNN.py
+++ testNN.py
@@ -16,6 +16,7 @@ dot_product_2 = np.dot(input_vector, weights_2)
 #print(f"The dot product is: {dot_product_2}")
 '''
 import numpy as np
+import matplotlib.pyplot as plt
 '''
 #hard coding the network iteratively
 #wrapping the vectors in NumPy arrays
@@ -86,7 +87,42 @@ class NeuralNetwork:
         self.bias = self.bias - (derror_dbias * self.learning_rate)
         self.weights = self.weights - (derror_dweights * self.learning_rate)
 
+    def train(self, input_vectors, targets, iterations):
+        cumulative_errors = []
+        for current_iteration in range(iterations):
+            # Pick a data instance at random
+            random_data_index = np.random.randint(len(input_vectors))
 
+            input_vector = input_vectors[random_data_index]
+            target = targets[random_data_index]
+
+            # Compute the gradients and update the weights
+            derror_dbias, derror_dweights = self._compute_gradients(
+                input_vector, target
+            )
+
+            self._update_parameters(derror_dbias, derror_dweights)
+
+            # Measure the cumulative error for all the instances
+            if current_iteration % 100 == 0:
+                cumulative_error = 0
+                # Loop through all the instances to measure the error
+                for data_instance_index in range(len(input_vectors)):
+                    data_point = input_vectors[data_instance_index]
+                    target = targets[data_instance_index]
+
+                    prediction = self.predict(data_point)
+                    error = np.square(prediction - target)
+
+                    cumulative_error = cumulative_error + error
+                cumulative_errors.append(cumulative_error)
+
+        return cumulative_errors
+    
+    
+
+#basic tests below without using train method
+'''
 if __name__ == "__main__":
     nn = NeuralNetwork(learning_rate = 0.1)
     input_vector = np.array([2, 1.5])
@@ -97,3 +133,27 @@ if __name__ == "__main__":
         print(f"Step{i}: Prediction = {pred:.4f}")
         db, dw = nn._compute_gradients(input_vector, target)
         nn._update_parameters(db, dw)
+        '''
+
+#tests using the train mmethod
+
+input_vectors = np.array([[3, 1.5], [2,1], [4, 1.5], [3,4], [3.5, 0.5], [2, 0.5], [5.5, 1], [1,1]])
+targets = np.array([0,1,0,1,0,1,1,0])
+learning_rate = 0.1
+
+neural_network = NeuralNetwork(learning_rate)
+training_error = neural_network.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
+
+
+def test_model(model, inputs):
+        for i,x in enumerate(inputs):
+            pred = model.predict(x)
+            print(f"Test input {i}: {x}, Prediction: {pred:.4f}")
+
+new_inputs = np.array([ [3,2] , [1,0.5] , [4,4] ])
+test_model(neural_network, new_inputs)
